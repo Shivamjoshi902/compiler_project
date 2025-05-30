280bd7714779b498c57f21cde63087b683c80124
sid2127 <157628463+sid2127@users.noreply.github.com>
2025-05-30 22:39:43 +0530

    Update targetcode.py

--- targetcode.py
+++ targetcode.py
@@ -1,1 +1,42 @@
+from lexer import lexer 
+from parser import parser
+from semantic import SemanticAnalyzer
+from ir_generator import IRGenerator
+from optimizer import Optimizer
+from executor import Executor
+import io
+import sys
 
+def run_code_with_compiler(user_code: str):
+    
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = io.StringIO()
+
+    try:
+        
+        lexer.input(user_code)
+
+       
+        ast = parser.parse(user_code, lexer=lexer)
+
+        
+        SemanticAnalyzer().analyze(ast)
+
+       
+        ir = IRGenerator()
+        ir.generate(ast)
+
+       
+        opt = Optimizer(ir.code)
+        opt.optimize()
+
+       
+        result = Executor(opt.get_code)
+        result.run()
+    except Exception as e:
+        print("error", e)
+
+    sys.stdout = old_stdout
+
+    
+    return mystdout.getvalue()
